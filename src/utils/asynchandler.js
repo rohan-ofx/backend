4d505eaadb2ed1f8b55e82cc2da94a13be601b8b
Rohan singh <rohansingh822004@gamil.com>
2026-09-08 01:15:57 +0530

    utils

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,27 @@
+const asynchandler = (requesthandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+export {asynchandler}
+
+//TRY  CATCH PROCESS
+
+
+// const asynchandler =(fn) =>async(req , res , next)=> {
+//     try {
+//         await fn(req, res, next)
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message: err.message
+//         })
+        
+//     }
+
+// }
